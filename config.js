56105e7a4c2c2f98327c7d911908989315d2432b
vercel-24-7 <rasathemi@gmail.com>
2026-07-07 10:45:37 +0530

    Change default SESSION_ID in config.js
    Updated SESSION_ID to a new default value.

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT_YOUR_SESSON-ID_HERE",
+SESSION_ID: process.env.SESSION_ID || "UBNlGIoL#uK2QS7m7c0zsdL4gthnCQ-_lbRv4FvinhSfLdePUsRo",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
